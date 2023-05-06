5e920a57254cc5487eb878a099cdb2877538f3ec
John Vu <johntrinhvu@yahoo.com>
2023-05-05 20:26:49 -0700

    Added partial templates

--- controllers/skills.js
+++ controllers/skills.js
@@ -10,14 +10,16 @@ module.exports = {
 
 function index(req, res) {
     res.render('skills/index', {
-      skills: Skill.getAll()
+      skills: Skill.getAll(),
+      title: 'All Skills'
 
     });
 }
 
 function show(req, res) {
     res.render('skills/show', {
-        skill: Skill.getOne(req.params.id)
+        skill: Skill.getOne(req.params.id),
+        title: 'Skill Details'
         
     });
 }
